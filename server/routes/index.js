d709a5a683fbfcefc52932e1c4b63bd94813136a
Henrik Johansson <khvjohansson@gmail.com>
2019-03-05 01:35:43 +0100

    first prototype done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
+
+var username = process.env.BNET_CLIENT_ID;
+var secret = process.env.BNET_SECRET;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/getCharSettings', function(req, res, next) {
+  fetch("https://" + username + ":" + secret + "@eu.battle.net/oauth/token?grant_type=client_credentials")
+    .then(token_response => token_response.json())
+    .then(token_response => {
+      console.log(token_response)
+      console.log(token_response.access_token)
+      console.log(req.param("charName") + " + " + req.param("realm"))
+      if(!(req.param("charName") && req.param("realm"))) {
+        res.status(400)
+        res.send("Bad request")
+        return
+      }
+      fetch("https://eu.api.blizzard.com/wow/character/" + req.param("realm") + "/" + req.param("charName") + "?fields=appearance&access_token=" + token_response.access_token, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(response => {
+          console.log(response)
+          res.json(response)
+          return
+        })
+        .catch(e => {
+          console.log(e)
+          res.status(500)
+          res.send("Error")
+          return
+        })
+    })
+    .catch(e => {
+      console.log(e)
+      res.status(500)
+      res.send("error")
+      return
+    })
 });
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      charName: null,
+      realm: null,
+      char: null,
+    }
+
+    this.handleCharChange = this.handleCharChange.bind(this);
+    this.handleRealmChange = this.handleRealmChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleCharChange(event) {
+    this.setState({charName: event.target.value});
+  }
+
+  handleRealmChange(event) {
+    this.setState({realm: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.realm)
+    fetch("http://localhost:4000/api/getCharSettings?realm=" + this.state.realm + "&charName=" + this.state.charName, {
+      method: "POST"
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        this.setState({
+          char: res
+        })
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h1>Wow character settings extractor</h1>
+          <div id="contentContainer">
+            <div id="formContainer">
+              <h3>Input</h3>
+              <form onSubmit={this.handleSubmit}>
+                <label for="charNameInput">
+                  Character <input id="charNameInput" className="input" type="text" value={this.state.charName} onChange={this.handleCharChange}></input>
+                </label><br/>
+                <label for="realmInput">
+                  Realm <input id="realmInput" className="input" type="text" value={this.state.realm} onChange={this.handleRealmChange}></input>
+                </label><br/>
+                <input className="submit" type="submit" value="Get info" />
+              </form>
+            </div>
+            <div id="resultContainer">
+              <h3>{this.state.char ? this.state.char.name : "Woop Woop!"}</h3>
+              <h4>{this.state.char ? this.state.char.realm : ""}</h4>
+              
+                {this.state.char ?
+                  <table>
+                    <tr>
+                      <th className="charLabels">Face:</th>
+                      <td>{this.state.char.appearance.faceVariation + 1}</td>
+                    </tr>
+                    <tr>
+                      <th className="charLabels">Skin color:</th>
+                      <td>{this.state.char.appearance.skinColor + 1}</td>
+                    </tr>
+                    <tr>
+                      <th className="charLabels">Hairstyle:</th>
+                      <td>{this.state.char.appearance.hairVariation + 1}</td>
+                    </tr>
+                    <tr>
+                      <th className="charLabels">Feature:</th>
+                      <td>{this.state.char.appearance.featureVariation + 1}</td>
+                    </tr>
+                  </table>
+                  : ""
+                }
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
